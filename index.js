539b773487f6a00fd051ce6ab3bffa27fc3aceda
Krimskii <krimskii@hotmail.com>
2018-01-18 16:45:21 +0600

    initial

--- index.js
+++ index.js
@@ -4,14 +4,55 @@ const TOKEN = '448727496:AAHLPsVhd272Elei-pM35kTKv6hVPuflHjQ'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
+const KB = {
+	news: 'Новости',
+	request: 'Обращение',
+	cat: "КотЭ",
+	car: "Машина",
+	back: "Назад"
+}
+
 bot.onText(/\/start/, msg =>{
 	const text = `Добрый день, ${msg.from.first_name}\nЧто вы хотите?`
 
 	bot.sendMessage(msg.chat.id, text, {
 		reply_markup: {
 			keyboard: [
-				['Новости', 'Обращение']
+				['KB.news', 'KB.request']
 			]
 		}
 	})
 })
+
+bot.on('message', msg => {
+	switch (msg.text) {
+		case KB.news:
+			sendPictureScreen(msg.chat.id)
+			break
+		case KB.request
+			break
+		case KB.back:
+			bot.sendMessage(msg.chat.id, `Что вы хотите??`, {
+				reply_markup: {
+			keyboard: [
+				['KB.news', 'KB.request']
+			]
+		}
+			})
+			break
+		case KB.car
+		case KB.cat
+			break
+	}
+})
+
+function sendPictureScreen(chatId) {
+	bot.sendMessage(chatId, `Выберите тип: `, {
+		reply_markup : {
+			keyboard: [
+				[KB.car, KB.cat]
+				[KB.back]
+			]
+		}
+	})
+}
