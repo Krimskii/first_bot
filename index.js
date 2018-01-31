bd166b7dd829fd8df69f607e0a5bbd8e46533680
krimskii <Krimskii@hotmail.com>
2018-01-31 18:14:33 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ bot.on('message', msg => {
 })
 
 function sendGreeting(msg, sayHello = true) {
-	const text = sayHello ?
-	`Добрый день, ${msg.from.first_name}\nВас приветствует БОТ общественной приемной акимата г.Алматы`
+	const text = sayHello
+	? `Добрый день, ${msg.from.first_name}\nВас приветствует БОТ общественной приемной акимата г.Алматы`
 	: `Воспользуйтесь меню` + emoji.get('point_down')
 	bot.sendMessage(msg.chat.id, text, {
 		reply_markup: {
